d811d71f03dde38949170710eed3d42c8827e679
Francisco García <88851030+JFranciscoGR03@users.noreply.github.com>
2021-09-03 11:50:37 -0500

    Solved!

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -2,14 +2,20 @@ def main():
     #escribe tu código abajo de esta línea
     val=int(input())
     cont=val//2
-    if val!=0:
-        for i in range(cont):
-            print("#")
-            for i in range(1):
-                print("%")
+    lista=1
 
-    if val%2!=0:
-        print("#")
+    for x,y in enumerate(str(lista)):
+        if val!=0:
+            for i in range(cont):
+                print(lista,"-","#")
+                lista+=1
+                for i in range(1):
+                    print(lista,"-","%")
+                    lista+=1
+
+        if val%2!=0:
+            print(lista,"-","#")
+            lista+=1
 
 
 if __name__=='__main__':   
