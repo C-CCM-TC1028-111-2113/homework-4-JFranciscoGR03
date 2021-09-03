9a1af4e9a2ec61c4be90f786620f831b95ebd8e0
Francisco García <88851030+JFranciscoGR03@users.noreply.github.com>
2021-09-03 10:00:06 -0500

    Solved!

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,6 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    val=int(input())
+    cont=val//2
+    if val!=0:
+        for i in range(cont):
+            print("#")
+            for i in range(1):
+                print("%")
+
+    if val%2!=0:
+        print("#")
+
 
 if __name__=='__main__':   
     main()
