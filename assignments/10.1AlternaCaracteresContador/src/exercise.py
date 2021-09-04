6ff1f500a7fe93e09d3360c189714f888eae0dd5
Francisco García <88851030+JFranciscoGR03@users.noreply.github.com>
2021-09-04 00:23:03 -0500

    Solved!

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,7 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
-    val = int(input())
+    val = int(input("ingresa un numero"))
     cont=0
+    #for cont in range(1,val+1):
     while cont<val:
         cont+=1
         if cont%2!=0:
