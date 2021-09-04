3a2912bdc207cd3ebaddc9a45c24f1baa4c70c0d
Francisco García <88851030+JFranciscoGR03@users.noreply.github.com>
2021-09-04 00:17:32 -0500

    Solved!

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,21 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
     val = int(input())
-    lista=1
-    cont=val//2
-
-    while lista<val:
-        for i in range(cont):
-            print(str(lista) + "-#")
-            lista+=1
-            for i in range(1):
-                print(str(lista) + "-%")
-                lista+=1
-
-    if val%2!=0:
-        print(str(lista) + "-#")
-        lista+=1
-
+    cont=0
+    #for cont in range(1,val+1):
+    while cont<val:
+        cont+=1
+        if cont%2!=0:
+            print(str(cont)+"-#")
+        else:
+            print(str(cont)+"-%")
 
 if __name__=='__main__':   
     main()
